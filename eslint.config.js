447d5e44439092e4c6528ecbc7d41d6bd1f942bf
KevinGastelum <kev.gas777@gmail.com>
2026-06-09 17:39:49 -0700

    feat(obs-server): scaffold Bun server package + ESLint ignore (C8)

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'observability/server']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
